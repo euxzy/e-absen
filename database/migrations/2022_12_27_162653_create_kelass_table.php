cf74cbfe2d0734fcf97e9e49fe908f4e39026e86
euxzy <rijal.m12l@gmail.com>
2022-12-27 23:35:11 +0700

    [ + ] create model and migration kelas

---
+++ database/migrations/2022_12_27_162653_create_kelass_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kelass', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_wali_kelas');
+            $table->tinyInteger('tingkat');
+            $table->string('nama', 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kelass');
+    }
+};
