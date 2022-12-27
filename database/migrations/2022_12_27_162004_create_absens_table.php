58f95900bb0b184282fa9cf41e7631a141e1068b
euxzy <rijal.m12l@gmail.com>
2022-12-27 23:24:55 +0700

    [ + ] create model and migration absen

---
+++ database/migrations/2022_12_27_162004_create_absens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absens', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_siswa');
+            $table->tinyInteger('status');
+            $table->text('keterangan');
+            $table->text('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('absens');
+    }
+};
