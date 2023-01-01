19c60f7f57bc4cc3f47cddba216fe9e8a77fe883
euxzy <rijal.m12l@gmail.com>
2023-01-01 23:36:56 +0700

    [ + ] update sidebar

--- resources/views/livewire/partials/sidebar.blade.php
+++ resources/views/livewire/partials/sidebar.blade.php
@@ -5,30 +5,36 @@
 
     <ul class="flex flex-col justify-center gap-1 pt-4 text-xs font-semibold text-[#364356]">
       <li>
-        <a href="{{ route('home') }}" class="inline-block w-full rounded-xl bg-[#5667FD]/30 px-4 py-3">Dashboard</a>
+        <a href="{{ route('home') }}"
+          class="{{ request()->routeIs('home') ? 'bg-[#5667FD]/30' : '' }} inline-block w-full rounded-xl px-4 py-3 hover:bg-[#5667FD]/30">Dashboard</a>
       </li>
 
       <template x-if="role == 2">
         <div class="flex flex-col justify-center gap-1">
           <li>
             <a href="{{ route('dashboard.admin.add') }}"
-              class="inline-block w-full rounded-xl px-4 py-3 hover:bg-[#5667FD]/30">Tambah Admin</a>
+              class="{{ request()->routeIs('dashboard.admin.add') ? 'bg-[#5667FD]/30' : '' }} inline-block w-full rounded-xl px-4 py-3 hover:bg-[#5667FD]/30">Tambah
+              Admin</a>
           </li>
           <li>
             <a href="{{ route('dashboard.walkel.add') }}"
-              class="inline-block w-full rounded-xl px-4 py-3 hover:bg-[#5667FD]/30">Tambah Wali Kelas</a>
+              class="{{ request()->routeIs('dashboard.walkel.add') ? 'bg-[#5667FD]/30' : '' }} inline-block w-full rounded-xl px-4 py-3 hover:bg-[#5667FD]/30">Tambah
+              Wali Kelas</a>
           </li>
           <li>
             <a href="{{ route('dashboard.walkel.list') }}"
-              class="inline-block w-full rounded-xl px-4 py-3 hover:bg-[#5667FD]/30">List Wali Kelas</a>
+              class="{{ request()->routeIs('dashboard.walkel.list') ? 'bg-[#5667FD]/30' : '' }} inline-block w-full rounded-xl px-4 py-3 hover:bg-[#5667FD]/30">List
+              Wali Kelas</a>
           </li>
           <li>
             <a href="{{ route('dashboard.siswa.add') }}"
-              class="inline-block w-full rounded-xl px-4 py-3 hover:bg-[#5667FD]/30">Tambah Siswa</a>
+              class="{{ request()->routeIs('dashboard.siswa.add') ? 'bg-[#5667FD]/30' : '' }} inline-block w-full rounded-xl px-4 py-3 hover:bg-[#5667FD]/30">Tambah
+              Siswa</a>
           </li>
           <li>
             <a href="{{ route('dashboard.siswa.list') }}"
-              class="inline-block w-full rounded-xl px-4 py-3 hover:bg-[#5667FD]/30">List Siswa</a>
+              class="{{ request()->routeIs('dashboard.siswa.list') ? 'bg-[#5667FD]/30' : '' }} inline-block w-full rounded-xl px-4 py-3 hover:bg-[#5667FD]/30">List
+              Siswa</a>
           </li>
         </div>
       </template>
